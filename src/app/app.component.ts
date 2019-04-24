8d54bdfb6206ff58c78d8c42bc50f6d26e07f206
Young Liu <youngliu92@gmail.com>
2019-04-24 16:09:36 -0700

    add ability to create furniture objects out of the observable.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from "rxjs/Observable";
 import { FurnitureService } from "./furniture.service"
+import { Furniture } from './models/furniture.model';
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [FurnitureService]
 })
-export class AppComponent {
-  constructor(private furnitureService: FurnitureService) { }
+export class AppComponent implements OnInit {
+
+  furnitureObs;
+  furnitureList: Furniture[] = [];
+  displayTable: boolean = false;
+
+  constructor(private furnitureService: FurnitureService) {
+  }
+
+  ngOnInit() {
+    this.getFurniture();
+  }
+
+  getFurniture() {
+    this.furnitureService.getFurnitureData().subscribe(response => {
+      this.furnitureObs = response.json();
+      for (let furniture of this.furnitureObs.body.data) {
+        console.log(furniture)
+        if (furniture.dimensions === undefined) {
+          this.furnitureList.push(new Furniture(furniture.id, furniture.name, furniture.imageUrl, furniture.description, furniture.colors, furniture.type, furniture.deliverable, furniture.cost, furniture.stock));
+        }
+        else {
+          this.furnitureList.push(new Furniture(furniture.id, furniture.name, furniture.imageUrl, furniture.description, furniture.colors, furniture.type, furniture.deliverable, furniture.cost, furniture.stock, furniture.dimensions.length, furniture.dimensions.width));
+        }
+      }
+      console.log(this.furnitureList);
+    });
+  }
 }
